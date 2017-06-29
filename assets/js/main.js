e6c849c709462ae4e652e051a6814c701168fe5d
romina <romina.torres.b@gmail.com>
2017-06-29 10:43:31 -0400

    agrego js

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+	$('#agregar-tareas').html(localStorage.getItem('listItems'));
+	$('#tareas').html(localStorage.getItem('listItemsDos'));
+
+	$('body').keyup(function(e) {
+
+	    if(e.keyCode == 13) {
+	    	var tarea = $("#tarea").val();
+
+	        if(tarea == ""){
+				alert("Añade una tarea");
+			}else{
+				$("#agregar-tareas").append('<div class="padre"><input type="checkbox" id="tarea-nueva" class="la-tarea" /><label for="tarea-nueva">' + tarea + '</label><a class="waves-effect waves-light right btn sacar">remove</a><br><div>');
+				localStorage.setItem('listItems', $('#agregar-tareas').html());
+	        	$("#tarea").val("");	
+			}	                
+	    }
